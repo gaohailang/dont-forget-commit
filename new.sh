6b332cd2ceea0852b6c41c67672b4122c6ff022e
gaohailang <ghlndsl@126.com>
2016-03-30 09:36:44 +0800

    feat: add new shell to fake commit for fun~

---
+++ new.sh
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Author: gaohailang
+# @Date:   2016-03-30 09:06:49
+# @Last Modified by:   gaohailang
+# @Last Modified time: 2016-03-30 09:34:58
+
+for i in {1..360}
+do
+  echo $i
+  dateOpt="$i day ago"
+  echo "$dateOpt"
+  dateStr=`gdate --date "$dateOpt"`
+  echo "$dateStr" >> daily-fake-commits.txt
+  git commit --date="$dateStr" -am "chore: $dateStr"
+  echo "$dateStr\n"
+done
