debd195b6567a70d22a2a2834347a120e3ad3791
gaohailang <ghlndsl@126.com>
2016-05-02 17:07:42 -0600

    chore: Mon May  2 17:07:42 CST 2016

--- new.sh
+++ new.sh
@@ -2,9 +2,9 @@
 # @Author: gaohailang
 # @Date:   2016-03-30 09:06:49
 # @Last Modified by:   gaohailang
-# @Last Modified time: 2016-03-30 09:34:58
+# @Last Modified time: 2016-05-03 17:07:29
 
-for i in {1..2}
+for i in {1..20}
 do
   echo $i
   dateOpt="$i day ago"
